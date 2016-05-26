196b78f8868751f73063a3bcb322931d6f37020a
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2016-05-26 11:57:42 +0430

    Add instructions for working with git

--- hello.py
+++ hello.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
+# To write your first program, simply create a new branch using:
+#
+# git branch myhello
+# git checkout myhello
+#
+# Now, open up the nano text editor and add a print statement right after
+# the print statement at the bottom of this file, replacing My name (Mahyar)
+# with your name, and save it.
+# Then do the following:
+#
+# git add hello.py
+# git commit -m "Put your commit message here"
+# git push origin myhello
+
 print('Hello from Mahyar :)')
