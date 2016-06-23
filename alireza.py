fec60b6b1b706f8571a3ceea73285c027a6f6742
Alireza Mahmoudi <alir3za97@gmail.com>
2016-06-23 12:00:58 +0430

    add alireza.py

---
+++ alireza.py
@@ -0,0 +1,27 @@
+var = 3
+var # do nothing, but in shell can print var
+print (var)
+var = ':D' # because python is dynamically-typed language and java is not
+
+tup = (1.2, 3, False)
+
+print (tup[2])
+
+# tup[1] = ':)' tuples are immutable
+
+lst = ['alireza', 19, ':)']
+
+lst[1] = 20 #lists are mutable
+
+di = {  
+        "name" : "alireza", 
+        "age" : 19,
+        "grades" : {
+            "bekhanim" : 20, 
+            "benevisim" : 20,
+            "hedie haye asemani" : 20,
+            },
+        "graduated" : False,
+    }
+
+print (di)
