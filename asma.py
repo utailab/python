a4e8bc448dc1cc722c3742d640a46f32bd3ccbd5
Asma <asma.farajidzji@ut.ac.ir>
2016-06-25 15:30:51 +0430

    1.exercise-tuple-list-dict

---
+++ asma.py
@@ -0,0 +1,15 @@
+var  = 13 #In python threr is no need to declare variable type
+print(var)
+
+var = "python :D" #"""Because Python is a typeless language and when you set a va			with a particular type in make and object and assign var			by the address of it"""
+print(var)
+tup = (1.0, "tuple", True)
+print(tup[2])
+#if we set tup[2] 3 the programm errors because the tuple is an immutable type .
+lst = [45, "list", False]
+print(lst[2])
+lst[2] = "hello" #it doesnt error because the list is mutable type .
+print(lst[2])
+dic = {'Name' : "asma", 'grades' : "10, 12, 13", 'graduated' : True}
+print(dic['graduated'])
+
