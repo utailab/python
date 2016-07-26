60a5476949bd3da62f8ca31ec70358ac4779604d
Atena GM <ashenatena@gmail.com>
2016-07-26 09:25:23 +0430

    exercises for functions and control statements

--- atena.py
+++ atena.py
@@ -1,22 +1,45 @@
-var = 5
-print(var)
+# Atena G.Mohammadi
 
-var = "salam" # python is a typeless programming language
+def function():
+    var = 5
+    print(var)
+    
+    var = "salam" # python is a typeless programming language
 	# as a consequence, we can define anything of any type to a previously
 	# defined variable
-print(var)
+    print(var)
+    
+    tup = (3, 3.5, True)
+    print(tup[2])
+    
+    # tup[1] = 1 raises an error since a tuple is immutable
+    
+    lst = [1, -0.5, "Hi"]
+    print(lst[2])
+    
+    lst[1] = False # this does not raise an error 'cause list is mutable
+    print(lst)
+    
+    di = {"name":["atena", "jack"], "grades":[[19, 16.5, 15], [2, 6, 9.9]],
+    	"graduted":[True, False]}
+    print(di)
 
-tup = (3, 3.5, True)
-print(tup[2])
 
-# tup[1] = 1 raises an error since a tuple is immutable
+def F(start, end):
+    list = []
+    start = float(start)
+    end = float(end)
+    if(start > 0 and end > 0):
+        if(end >= start):
+            if(start - int(start) < 1):
+                start = int(start) + 1
+            else:
+                start = int(start)
+            end = int(end)
+            list = [i for i in range(start, end + 1)]
+    return list
 
-lst = [1, -0.5, "Hi"]
-print(lst[2])
 
-lst[1] = False # this does not raise an error 'cause list is mutable
-print(lst)
-
-di = {"name":["atena", "jack"], "grades":[[19, 16.5, 15], [2, 6, 9.9]],
-	"graduted":[True, False]}
-print(di)
+function()
+a = [input("Enter start: "), input("Enter end: ")]
+print(F(*a))
